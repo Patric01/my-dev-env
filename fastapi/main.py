04601c12a8ea6dcbd04591928f28829b49c75d06
Vlădulescu Victor Valentin <victor-valentin.vladulescu@endava.com>
2025-06-18 12:27:16 +0300

    Fixed CORS issue

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,8 +3,22 @@ from sqlmodel import Session
 from  models import Users
 from crud import get_users
 from database import get_session
+from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
+
+origins = [
+    "*"
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @api.get("/", response_model = list[Users])
 def index(session: Session = Depends(get_session)):
