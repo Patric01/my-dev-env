7a0f972777130934cd3da5444428aa7b1666e4f7
Vlădulescu Victor Valentin <victor-valentin.vladulescu@endava.com>
2025-07-09 14:42:38 +0300

    Changed backend base URL to localhost

--- angular/src/app/constants.ts
+++ angular/src/app/constants.ts
@@ -1,1 +1,5 @@
-export var backend_url_base = 'https://172.31.11.96:8000/';
+// For production, modify the backend URL to point to the production server
+// export var backend_url_base = 'https://172.31.11.96:8000/';
+
+// For development, use localhost
+export var backend_url_base = 'https://localhost:8000/';
